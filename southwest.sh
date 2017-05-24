87c174a06d5cd287c4452e0eadeca86ba2b1bc29
localstepdo <stepdo@microsoft.com>
2017-05-24 12:05:16 -0700

    shorten loop and add exit condition

--- southwest.sh
+++ southwest.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
+
+set -xe
+
 confirmationNumber=$1
 firstName=$2
 lastName=$3
-COUNTER=0
-while [  $COUNTER -lt 100000000 ]; do
+
+results_folder=/tmp/attempt
+rm -rf $results_folder
+mkdir -p $results_folder
+
+# Loop will run for over an hour.
+prevHash= #unitialized.
+COUNTER=1
+while [[  $COUNTER -lt 15000 ]]; do
 	sleep .25
-	echo Trying send data Confirmation Number: $1, First Name: $2, Last Name: $3
-	curl --data "confirmationNumber=${confirmationNumber}&firstName=${firstName}&lastName=${lastName}&submitButton=Check In" https://www.southwest.com/flight/retrieveCheckinDoc.html
-	echo Trying Southwest Checkin $COUNTER
+
+	# Message
+	echo "Trying Southwest Checkin. Attempt: $COUNTER"
+	echo "Confirmation Number: $1, First Name: $2, Last Name: $3"
+
+	# Web requests
+	#todo: this relies on html markup ids remaining unchanged.
+	curl --data "confirmationNumber=${confirmationNumber}&firstName=${firstName}&lastName=${lastName}&submitButton=Check In" https://www.southwest.com/flight/retrieveCheckinDoc.html > $results_folder/${COUNTER}.html
+
+	# Remove unique strings and get file hash.
+	sed -i -e 's/(.*SW.*)//g' $results_folder/${COUNTER}.html
+	curHash=`md5sum $results_folder/${COUNTER}.html | tr " " "\n" | head -1`
+
+	# Is this page different than the previous result?
+	# A different web response generally means success, but this is brittle. We could do much better
+	if [[ -n $prevHash ]] && [[ $prevHash != $curHash ]]; then
+		# Success!
+		echo "Successfully checked in!"
+		echo "See $results_folder for attempt results"
+		break;
+	fi
+
+	echo # Newline
+	prevHash=$curHash
 	let COUNTER=COUNTER+1 
 done
+
+# todo: does this work for multiple passengers on the same itinerary?
+# todo: does this automatically trigger the following "next" button?
